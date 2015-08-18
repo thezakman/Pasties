adcc9c772fc9fd920eb6008d62947ee7c1d1487a
TheZakMan <thezakman@icloud.com>
2015-08-18 04:16:59 -0300

    Added all the scripts

---
+++ clipboard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TheZakMan    [ 18/08/2015 04:01:11 ]  
+# https://github.com/thezakman/Pasties
+
+# ------------------------------------------------------------------------
+# Script para me salvar tempo copiado muitas linhas para o clipboard
+# -------------------------------------------------------------------------
+
+# [>]Como usar: "cat arquivo | clipboard" ou "echo -e "teste" | clipboard"
+
+xsel --clipboard
+
---
+++ termbin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# TheZakMan    [ 18/08/2015 04:01:11 ]  
+# https://github.com/thezakman/Pasties
+
+# ------------------------------------------------------------------------
+# Script para me salvar tempo exportando code/text para o termbin
+# -------------------------------------------------------------------------
+
+# [>]Como usar: "cat arquivo | termbin" ou "echo -e "teste" | termbin"
+
+
+amarelo='\033[1;33m' # Amarelo
+SemCor='\033[0m' # Sem Cor
+
+# ---------------------------- # confirma se o netcat foi instalado na maquina
+
+if test -e /bin/netcat ; then
+    echo "____________________" >&2
+else
+    echo "Dependencia faltando: apt-get install netcat" >&2      
+    apt-get install netcat -y
+    exit 1
+    fi
+
+# ------------------------------# Exec
+
+echo -e "${amarelo}[>]Gerando o link...${SemCor}" ; nc termbin.com 9999 
